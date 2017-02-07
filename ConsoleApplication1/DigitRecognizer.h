01096bdb2658e0669258b8da2d257022e961b337
David <dale7642@gmail.com>
2017-02-07 14:52:06 -0800

    Add project files.

---
+++ ConsoleApplication1/DigitRecognizer.h
@@ -0,0 +1,20 @@
+#include <opencv/cv.h> 
+#include <opencv/highgui.h> 
+#include <opencv/ml.h> 
+
+using namespace cv;
+#define MAX_NUM_IMAGES 60000 
+
+class DigitRecognizer {
+public:
+	DigitRecognizer();
+	~DigitRecognizer();
+	bool train(char* trainPath, char* labelsPath);
+	int classify(Mat img);
+private:
+	Mat preprocessImage(Mat img);
+	int readFlippedInteger(FILE *fp);
+private:
+	KNearest    *knn;
+	int numRows, numCols, numImages;
+};
